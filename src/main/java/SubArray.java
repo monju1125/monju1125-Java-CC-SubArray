73fbc05dfa9c3b2853fd7c8b09e472a8ac7c5fcf
monju1125 <monju1125@github.com>
2026-06-27 22:59:05 +0000

    Test run: 0/3 passed, 3 failed, 0 skipped - 2026-06-27T22:59:05.959Z

--- src/main/java/SubArray.java
+++ src/main/java/SubArray.java
@@ -13,6 +13,10 @@ public class SubArray {
      * @return a sub-array of nums containing the values between start and end.
      */
     public int[] sub(int[] nums, int start, int end){
-        return null;
+        int[] SubArray =  new int[end-start+1];
+        for(int i = 0; i < nums.length; i++){
+                SubArray[i] = nums[i];
+        }
+        return SubArray;
     }
 }
